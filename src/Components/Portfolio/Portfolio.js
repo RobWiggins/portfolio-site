2272f3004213680345b9f627229ac14a2a8b8c43
Robert Wiggins <wigginsro11@gmail.com>
2019-09-04 15:20:35 -0400

    added barometer desktop screenshot

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export default class Portfolio extends Component {
   render() {
     return (
       <section className="portfolio-info">
         <h1>Portfolio</h1>
+        <img
+          className="project-screenshot barometer-screenshot"
+          src="../../static/barometer_full.png"
+          alt="barometer home search page screenshot, containing a list of tweets and a bar graph of different emotions."
+        ></img>
+
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
           deserunt, doloremque consequatur enim ducimus dolorum molestiae, nobis
@@ -16,11 +22,11 @@ export default class Portfolio extends Component {
           non repudiandae tempora ex ut tempore inventore perspiciatis?
           Distinctio, non!
         </p>
-        
+
         <Link to="/contact">
           <button className="call-action">Talk to me!</button>
         </Link>
       </section>
-    );
+    )
   }
 }
